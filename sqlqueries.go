23689de518c01e24a6f95b4c3e553dba451f4761
BumbleB-NL <102973103+BumbleB-NL@users.noreply.github.com>
2022-04-04 15:44:17 +0200

    Initial commit

---
+++ sqlqueries.go
@@ -0,0 +1,120 @@
+package sqlqueries
+
+import (
+	"crypto/sha1"
+	"database/sql"
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func getEnvVariables(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	return os.Getenv(key)
+}
+
+func hash(username string, password string) (hash string) {
+	bytes := []byte(username + password)
+	hasher := sha1.New()
+	hasher.Write(bytes)
+	hash = hex.EncodeToString((hasher.Sum(nil)))
+	return hash
+}
+
+func SqlConnect() (db *sql.DB) {
+	usernameSQL := getEnvVariables("DB_USERNAME")
+	passwordSQL := getEnvVariables("DB_PASSWORD")
+	hostnameSQL := getEnvVariables("DB_HOSTNAME")
+	tablenameSQL := getEnvVariables("DB_TABLENAME")
+	db, err := sql.Open("mysql", usernameSQL+":"+passwordSQL+"@tcp("+hostnameSQL+")/"+tablenameSQL)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func SqlRegister(db *sql.DB, username string, hash string, email string) (allowed bool) {
+	insertQuery := "INSERT INTO `users` (Username, Email, Authentication) VALUES (?, ?, ?)"
+	insert, err := db.Query(insertQuery, username, email, hash)
+	defer insert.Close()
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func SqlLogin(db *sql.DB, username string, hashed string) (allowed bool) {
+	loginQuery := "SELECT Authentication FROM `users` WHERE Username = ?"
+	login, err := db.Query(loginQuery, username)
+	defer login.Close()
+	if err != nil {
+		panic(err)
+	}
+	login.Next()
+
+	var passwordCheck string
+	login.Scan(&passwordCheck)
+
+	if hashed == passwordCheck {
+		fmt.Println("You are in da mainframe")
+		return true
+	} else {
+		fmt.Println("Authentication error")
+		return false
+	}
+}
+
+func SqlCreateToken(db *sql.DB, username string, hashed string) (allowed bool) {
+	createTokenQuery := "UPDATE `users` SET Token = ? WHERE Username = ?"
+	rand.Seed(time.Now().UnixNano())
+	random := strconv.Itoa(rand.Intn(1000))
+	token, err := db.Query(createTokenQuery, hash(hashed, random), username)
+	defer token.Close()
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func SqlDeleteToken(db *sql.DB, username string) (allowed bool) {
+	deleteTokenQuery := "UPDATE `users` SET Token = NULL WHERE Username = ?"
+	delete, err := db.Query(deleteTokenQuery, username)
+	defer delete.Close()
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func SqlGetToken(db *sql.DB, username string) (allowed bool, tokenString string) {
+	getTokenQuery := "SELECT Token FROM `users` WHERE Username = ?"
+	token, err := db.Query(getTokenQuery, username)
+	defer token.Close()
+	if err != nil {
+		return false, ""
+	} else {
+		token.Next()
+
+		var tokenString string
+		token.Scan(&tokenString)
+
+		if tokenString == "" {
+			return false, tokenString
+		} else {
+			return true, tokenString
+		}
+	}
+}
